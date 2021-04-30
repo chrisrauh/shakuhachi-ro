86570316e7f4f39e09f92096caa79319a3ef304f
Christian Rauh <crauh@uber.com>
2021-04-29 19:35:34 -0700

    Better configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'semistandard'
+    'semistandard',
+    'plugin:prettier/recommended',
   ],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'sort-imports-es6-autofix'
-  ],
+  plugins: ['react', 'sort-imports-es6-autofix'],
   rules: {
-
     // React hooks rules
     // Checks rules of Hooks
     'react-hooks/rules-of-hooks': 'error',
@@ -34,19 +31,24 @@ module.exports = {
     // Checks effect dependencies
     'react-hooks/exhaustive-deps': 'warn',
 
-    'sort-imports-es6-autofix/sort-imports-es6': [2, {
-      ignoreCase: true,
-      ignoreMemberSort: false,
-      memberSyntaxSortOrder: ['none', 'multiple', 'single', 'all']
-    }],
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'multiple', 'single', 'all'],
+      },
+    ],
 
     'comma-dangle': ['error', 'only-multiline'],
-    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never', asyncArrow: 'always' },
+    ],
 
     'react/react-in-jsx-scope': 'off',
 
     // TODO: Remove this line to enable the rule after converting to Typescript
     'react/prop-types': [0, {}],
-
-  }
+  },
 };
